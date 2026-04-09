1ba493b9167d4b728cbabc3138b3162c38760619
Maaz Haider <maazhaider0011@gmail.com>
2026-04-10 01:30:32 +0500

    Delete issm-common-database-setup directory

--- issm-common-database-setup/issm_common_database_setup/mongo/__init__.py
+++
@@ -1,4 +0,0 @@
-from issm_common_database_setup.mongo.base_model import BaseModel
-from issm_common_database_setup.mongo.create_database import BeanieDBClient
-
-__all__ = ["BaseModel", "BeanieDBClient"]
